3a42dc69d829ac8efe0dd348b28af9a0159d6c46
nkleinmann <nekleinmann@gmail.com>
2021-03-01 21:25:23 -0500

    look-and-say-sequence

--- Topic-8/03-look-say-sequence/Unsolved/look-and-say-sequence.js
+++ Topic-8/03-look-say-sequence/Unsolved/look-and-say-sequence.js
@@ -2,12 +2,26 @@
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lookAndSay = function(n) {
     console.log(n);
-    n.toString();
-    console.log(n.toString().split(""));
-    let newArray = n.toString().split("");
-    for (let i=0; i<newArray.length; i++) {
-        newArray.forEach( element =>
-            console.log(newArray[i].indexOf(element)));
+    let numString = n.toString();
+    let result = "";
+
+    let currentCount = 1;
+    let currentDigit = numString[0];
+    for (let i=1; i <= numString.length; i++) {
+        let digit = numString[i];
+
+        if (digit === currentDigit){
+            currentCount++;
+        } else {
+            result += currentCount;
+            result += currentDigit;
+            console.log(`currentCount ${currentCount}`);
+            console.log(`currentDigit ${currentDigit}`);
+            currentCount = 1;
+            currentDigit = digit;
+        }
     }
+    console.log(result);
+    return parseInt(result);
 
 };
